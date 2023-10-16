6b3f42900d62fe46cd04e10ac98114fa22f73258
Moritz Windoffer <moritz.windoffer@gmail.com>
2023-10-16 22:35:24 +0200

    match entities prog

--- generate entities.py
+++ generate entities.py
@@ -15,23 +15,28 @@ PREFIX wkg: <http://www.worldkg.org/resource/>
 PREFIX wkgs: <http://www.worldkg.org/schema/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-SELECT ?item ?name ?pos
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+SELECT ?item ?name ?pos ?type
 WHERE {
 ?item wkgs:spatialObject ?obj.
 ?item rdfs:label ?name.
-?obj geo:asWKT ?pos
+?obj geo:asWKT ?pos.
+OPTIONAL { ?item rdf:type ?type .}
 }
 """
 
 print('- retrieving candidates')
 spatial_objects = []
 for r in g.query(spatial_object_query):
-    row = {'predicate': f"wkg:{r['item'].split('/')[-1]}", 'label': r['name'], 'location': r['pos']}
+    row = {'predicate': f"wkg:{r['item'].split('/')[-1]}",
+           'label': r['name'],
+           'location': r['pos'],
+           'type': r['type'].split('/')[-1] if r['type'] else '<UNK>'}
     spatial_objects.append(row)
 spat_obj_df = pd.DataFrame(spatial_objects)  # these spatial objects will be candidates
 # prefiltering would be possible
 
-spat_obj_df.to_parquet('candidates.parquet.gzip', compression='gzip', engine='pyarrow')
+spat_obj_df.to_parquet('candidates_type.parquet.gzip', compression='gzip', engine='pyarrow')
 
 # predefined spatial relations to scan for
 wkg_relations = pd.read_csv('relations.csv')
@@ -42,11 +47,13 @@ PREFIX wkg: <http://www.worldkg.org/resource/>
 PREFIX wkgs: <http://www.worldkg.org/schema/>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX geo: <http://www.opengis.net/ont/geosparql#>
-SELECT ?item ?o ?pos
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+SELECT ?item ?o ?pos ?type
 WHERE {
 ?item wkgs:%s ?o.
 ?item wkgs:spatialObject ?geoObj.
-?geoObj geo:asWKT ?pos 
+?geoObj geo:asWKT ?pos .
+OPTIONAL { ?item rdf:type ?type .}
 }
 """
 
@@ -56,8 +63,12 @@ pbar = tqdm(wkg_relations['relations'])
 for relation in pbar:
     pbar.set_postfix_str(relation.split(':')[-1])
     for r in g.query(query_relation % relation.split(':')[-1]):
-        row = {'subject': f"wkg:{r['item'].split('/')[-1]}", 'predicate': relation, 'literal': r['o'], 'location': r['pos']}
+        row = {'subject': f"wkg:{r['item'].split('/')[-1]}",
+               'predicate': relation,
+               'literal': r['o'],
+               'location': r['pos'],
+               'type': r['type'].split('/')[-1] if r['type'] else '<UNK>'}
         relation_list.append(row)
 relation_df = pd.DataFrame(relation_list)  # subjects to look for partner for (heads)
 
-relation_df.to_parquet('subjects.parquet.gzip', compression='gzip', engine='pyarrow')
+relation_df.to_parquet('subjects_type.parquet.gzip', compression='gzip', engine='pyarrow')
