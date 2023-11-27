2885721e1916dcf22a842d00d7ad7d9637811a4c
Moritz Windoffer <moritz.windoffer@gmail.com>
2023-11-27 09:37:10 +0100

    introduce cutoff to CreateTriples.py

--- CreateTriples.py
+++ CreateTriples.py
@@ -10,10 +10,10 @@ from datetime import timedelta
 import argparse
 
 class osm2rdf_handler(osmium.SimpleHandler):
-    def __init__(self):
+    def __init__(self, feature_file:str, key_file:str, min_tags:int):
         osmium.SimpleHandler.__init__(self)
-        self.pbar=tqdm(desc='- Processing Nodes')
-        self.counts=0
+        self.pbar = tqdm(desc='- Processing Nodes')
+        self.counts = 0
 
         # prepare Graph namespace
         self.g = Graph()
@@ -31,9 +31,9 @@ class osm2rdf_handler(osmium.SimpleHandler):
         self.rdfs = Namespace('http://www.w3.org/2000/01/rdf-schema#')
         self.g.bind("rdfs", self.rdfs)
         self.rdf = Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
-        self.g.bind('rdf',self.rdf)
+        self.g.bind('rdf', self.rdf)
         self.ogc = Namespace("http://www.opengis.net/rdf#")
-        self.g.bind('ogc',self.ogc)
+        self.g.bind('ogc', self.ogc)
         self.sf = Namespace("http://www.opengis.net/ont/sf#")
         self.g.bind('sf', self.sf)
         self.osmn = Namespace("https://www.openstreetmap.org/node/")
@@ -42,22 +42,24 @@ class osm2rdf_handler(osmium.SimpleHandler):
         self.namespace = {key: uri for key, uri in self.g.namespaces()}
 
         # load osm features and keys
-        self.supersub = pd.read_csv(args.osm_features, sep='\t', encoding='utf-8')
-        self.key_list = pd.read_csv(args.key_list, sep='\t', encoding='utf-8')
+        self.supersub = pd.read_csv(feature_file, sep='\t', encoding='utf-8')
+        self.key_list = pd.read_csv(key_file, sep='\t', encoding='utf-8')
         self.key_list = list(self.key_list['key'])
         self.supersub = self.supersub.drop_duplicates()
         
         self.dict_class = self.supersub.groupby('key')['value'].apply(list).reset_index(name='subclasses').set_index('key').to_dict()['subclasses']
+
+        self.min_tags = min_tags
     
     def to_camel_case_class(self, word):
-        word = word.replace(':','_')
+        word = word.replace(':', '_')
         return ''.join(x.capitalize() or '_' for x in word.split('_'))
     
     def to_camel_case_classAppend(self, key, val):
         return self.supersub.loc[(self.supersub['value'] == val) & (self.supersub['key'] == key)]['appendedClass'].values[0]
     
     def to_camel_case_key(self, input_str):
-        input_str = input_str.replace(':','_')
+        input_str = input_str.replace(':', '_')
         words = input_str.split('_')
         return words[0] + "".join(x.title() for x in words[1:])
     
@@ -106,10 +108,8 @@ class osm2rdf_handler(osmium.SimpleHandler):
                 try:
                     country = o.split(':')[0]
                     ids = o.split(':')[1]
-                    #ids = urllib.parse.quote(o.split(':')[1])
                 except IndexError:
                     country = ''
-                    #ids = urllib.parse.quote(o)
                     ids = o
                 url = country+'.wikipedia.org/wiki/'+country+':'+ids
                 url = 'https://'+urllib.parse.quote(url)
@@ -126,28 +126,23 @@ class osm2rdf_handler(osmium.SimpleHandler):
 
     def node(self, n):
         self.pbar.update(1)
-        if len(n.tags)>1:
-            lat = str(n.location.lat)
-            lon = str(n.location.lon)
+        if len(n.tags) > self.min_tags:
             id = str(n.id)
 
 
 
             point = 'Point('+str(n.location.lon)+' '+str(n.location.lat)+')'
 
-            #self.printTriple(id, "lat", lat)
-            #self.printTriple(id, "long", lon)
             self.printTriple(id, "Point", point)
             self.printTriple(id, "osmLink", id)
 
-
-            for k,v in n.tags:
+            for k, v in n.tags:
 
                 val = str(v)
 
-                val=val.replace("\\", "\\\\")
-                val=val.replace('"', '\\"')
-                val=val.replace('\n', " ")
+                val = val.replace("\\", "\\\\")
+                val = val.replace('"', '\\"')
+                val = val.replace('\n', " ")
 
                 k = k.replace(" ", "")
 
@@ -160,6 +155,7 @@ parser.add_argument('--osm_features', type=str, default='required files/OSM_Onto
 parser.add_argument('--key_list', type=str, default='required files/Key_List.csv')
 parser.add_argument('--output_file', type=str, default='data/graph.ttl')
 parser.add_argument('--input_file', type=str, required=True)
+parser.add_argument('--min_tags', type=int, default=1)
 
 args = parser.parse_args()
 
@@ -173,11 +169,11 @@ print('Compute WorldKG Triples:')
 print(f'- reading from {args.input_file}')
 print(f'- will write to {args.output_file}')
 
-h = osm2rdf_handler()
+h = osm2rdf_handler(args.osm_features, args.key_list, args.min_tags)
 h.apply_file(args.input_file)
 h.pbar.close()
 print(f'- writing to {args.output_file}')
-h.graph.serialize(args.output_file, format="turtle", encoding="utf-8" )
+h.graph.serialize(args.output_file, format="turtle", encoding="utf-8")
 
 end = time.time()
 
