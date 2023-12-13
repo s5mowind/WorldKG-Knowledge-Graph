b4686f58f31f5ffa84ea216fa508aa3b80f60147
Moritz Windoffer <moritz.windoffer@gmail.com>
2023-12-13 21:07:37 +0100

    update with threaded writing for larger files

--- match entities.py
+++ match entities.py
@@ -6,6 +6,9 @@ from tqdm import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
 import argparse
 import re
+import csv
+from queue import Queue
+from threading import Thread
 
 def haversine_from_geohash(hash1:str, hash2:str) -> float:
     """
@@ -27,6 +30,7 @@ parser.add_argument('--geohash_precision', type=str, default='required files/geo
 parser.add_argument('--predicate_map', type=str, default='data/predicate_map.json')
 parser.add_argument('--literal_map', type=str, default='data/literal_map.json')
 parser.add_argument('--type_map', type=str, default='data/type_map.json')
+parser.add_argument('--start_value', type=int, default=0, help='number of predicates to skip, useful for testing or restarts')
 
 args = parser.parse_args()
 
@@ -93,9 +97,41 @@ for p in tqdm(set(geohash_precision_map.values()), desc='- Computing distance ma
         distance_frame = pd.DataFrame(1 - (distance_frame.values / max_val), columns=distance_frame.columns, index=distance_frame.index)
     distance_matrices.update({p: distance_frame})
 
+def consume(stop, queue, filename) -> None:
+    """
+    consumer function for threaded file writing
+    :param stop: function to define stop condition
+    :param queue: queue to read content from
+    :param filename: filename to write to
+    :return:
+    """
+    with open(filename, 'a', encoding='utf-8', newline='') as file:
+        writer = csv.writer(file, delimiter='\t')
+        while True:
+            if not queue.empty():
+                i = queue.get()
+                writer.writerow(i)
+            elif stop():
+                print('-stopping file writing thread')
+                return
+
+queue = Queue()
+stop_thread = False
+
+# if not a restart, empty or create output file
+if args.start_value == 0:
+    with open(args.output_file, 'w', encoding='utf-8', newline='') as file:
+        queue.put(['s', 'p', 'literal', 'o', 'score'])
+else:
+    print(f'- skinpping first {args.start_value} predicates:')
+    print(f"- {list(subjects['predicate'].value_counts().index[:args.start_value])}")
+
+# consumer for writing training data
+match_consumer = Thread(target=consume, daemon=True, args=(lambda: stop_thread, queue, args.output_file))
+match_consumer.start()
+
 matched = {}
-pairs = []
-pbar = tqdm(subjects['predicate'].value_counts().index, desc='Matching by predicate:')
+pbar = tqdm(subjects['predicate'].value_counts().index[args.start_value:], desc='Matching by predicate:')
 for relation in pbar:
     pbar.set_postfix_str(f'{relation}')
     selected_subjects = subjects[subjects['predicate'] == relation]
@@ -120,7 +156,7 @@ for relation in pbar:
             lit = row['literal']
             if (gh, pred, lit) in matched:  # only consider new constellations for subjects
                 # if these three values are the same, the uslp-score will also be the same
-                pairs.append((row['uri'], pred, lit) + matched[(gh, pred, lit)])
+                queue.put([row['uri'], pred, lit, matched[(gh, pred, lit)][0], matched[(gh, pred, lit)][1]])
             else:
                 # prepare similarity and distance matrices for repetitive access
                 dm_precision = distance_matrices[precision].loc[gh, :]
@@ -143,11 +179,10 @@ for relation in pbar:
                 best_candidate_score = candidate_eval[best_candidate]
 
                 # store matches and save constellation
-                pairs.append((row['uri'], pred, lit, best_candidate_uri, candidate_eval[best_candidate]))
+                queue.put([row['uri'], pred, lit, best_candidate_uri, candidate_eval[best_candidate]])
                 matched.update({(gh, pred, lit): (best_candidate_uri, best_candidate_score)})
 
-print(f'- {(len(subjects) - len(matched))/len(subjects)*100:.2f}% of computations performed with dictionary')
+stop_thread = True
+match_consumer.join()
 
-print(f'- saving results to {args.output_file}')
-predictions = pd.DataFrame(pairs, columns=['s', 'p', 'literal', 'o', 'score'])
-predictions.to_csv(args.output_file, index=False)
+print(f'- {(len(subjects) - len(matched))/len(subjects)*100:.2f}% of computations performed with dictionary')
